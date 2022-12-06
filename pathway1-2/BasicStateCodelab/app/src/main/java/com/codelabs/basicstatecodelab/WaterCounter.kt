3ba27e34057a33ade903517a496b95666c69dd55
강금실 <kanggumsil@deali.net>
2022-12-06 20:50:11 +0900

    pathway 1-2 완료

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,47 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
+            Text("You've had $count glasses.")
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text("Add one")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterCounterPreview() {
+    BasicStateCodelabTheme {
+        WaterCounter()
+    }
+}
